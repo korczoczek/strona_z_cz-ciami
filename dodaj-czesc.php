57473ee47827d2f18d6bf79f6522d8ca6774f439
korczoczek <danielkorczok@gmail.com>
2020-11-25 18:03:08 +0100

    tabelka jest

---
+++ dodaj-czesc.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+<?php
+    include('php/database-connect.php');
+    include('php/header.php');
+?>
+<title>tytuł</title>
+<style></style>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+    </div>
+</div>
+<?php
+    include('php/footer.php');
+    include('php/database-disconnect.php');
+?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -9,7 +9,31 @@
 <style></style>
 </head>
 <body>
-
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <table class="table">
+                <tbody>
+<?php
+    $sql = "SELECT zdjecie,nazwa,cena,ilosc,producent FROM magazyn;";
+    $result = $mysqli->query($sql);
+    while($czesci=$result->fetch_assoc()){
+        echo "
+        <tr>
+        <td>".$czesci['zdjecie']."</td>
+        <td>".$czesci['nazwa']."</td>
+        <td>".$czesci['cena']." zł</td>
+        <td>".$czesci['ilosc']."</td>
+        <td>".$czesci['producent']."</td>
+        </tr>
+        ";
+    }
+?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
 <?php
     include('php/footer.php');
     include('php/database-disconnect.php');
